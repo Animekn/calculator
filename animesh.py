68a59f9ef6505493968225a34c7c61cbaa7c96b0
unknown <animeshsngh68@gmail.com>
2024-01-05 14:19:01 +0530

    subtract implimented

--- animesh.py
+++ animesh.py
@@ -1,7 +1,9 @@
 def addNumbers(numberA, numberB):
     return numberA + numberB
-
+def subtractNumbers(numberA, numberB):
+    return numberA - numberB
 
 numberA = int(input("Enter a number: "))
 numberB = int(input("Enter another number: "))
 print("The sum of the two numbers is: ", addNumbers(numberA, numberB))
+print("The difference of the two numbers is: ", subtractNumbers(numberA, numberB))
