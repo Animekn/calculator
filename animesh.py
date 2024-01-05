a274615f0f63958d34bdf491dfcbd4a561a9b8eb
unknown <animeshsngh68@gmail.com>
2024-01-05 14:26:23 +0530

    done

--- animesh.py
+++ animesh.py
@@ -13,8 +13,6 @@ def multiply(a, b):
     return a * b
 def devide(a, b):
     return a / b
-numberA = int(input("Enter a number: "))
-numberB = int(input("Enter another number: "))
 print("The product is: ", multiply(numberA, numberB))
 print("The quotient is: ", devide(numberA, numberB))
 
