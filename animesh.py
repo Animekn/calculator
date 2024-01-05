e19f688f88b5c023c349b107d4539c71de31d33c
unknown <animeshsngh68@gmail.com>
2024-01-05 14:09:00 +0530

    function impliment

--- animesh.py
+++ animesh.py
@@ -1,3 +1,7 @@
+def addNumbers(numberA, numberB):
+    return numberA + numberB
+
+
 numberA = int(input("Enter a number: "))
 numberB = int(input("Enter another number: "))
-print("The sum of the two numbers is: ", numberA + numberB)
+print("The sum of the two numbers is: ", addNumbers(numberA, numberB))
